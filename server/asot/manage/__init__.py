a23b20baa2838d87df2b0df23b7cf21f6ce75de7
Luna <git@l4.pm>
2021-08-01 00:38:08 -0300

    add asot.manage module

---
+++ server/asot/manage/__init__.py
@@ -0,0 +1,3 @@
+# asot: Localhost tunneling
+# Copyright 2021, Luna and asot contributors
+# SPDX-License-Identifier: BSD-3-Clause
---
+++ server/asot/manage/__main__.py
@@ -0,0 +1,8 @@
+# asot: Localhost tunneling
+# Copyright 2021, Luna and asot contributors
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .main import main
+
+if __name__ == "__main__":
+    main()
